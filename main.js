f984f1769776116991aaaba119a05770866bc2f5
Ray <raycheltem@gmail.com>
2017-05-29 22:34:46 -0700

    complete add action inputs, onclick creates button

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ function addSkill() {
 	var newSkill = document.getElementById("skillsInput").value;
 	skillsArray.push(newSkill);
 	pointsArray.push(0);
+
 	// insert rows and cells for new skill and point counter
 	var table = document.getElementById("skillsTable");
 	var row = table.insertRow(skillsArray.length);
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
+
 	// put new skill and counter text in to new cells
 	cell1.innerHTML = newSkill;
 	cell2.innerHTML = 0;
@@ -20,4 +22,16 @@ function addSkill() {
 	opt.textContent = newSkill;
 	opt.value = newSkill;
 	select.appendChild(opt);
+}
+
+function addAction() {
+	var newAction = document.getElementById("ActionInputName").value;
+	var corrSkill = document.getElementById("ActionSelectSkill").value;
+	var pointValue = document.getElementById("ActionInputPoints").value;
+
+	// make button
+	var div = document.getElementById("actionsDiv");
+	var newButton = document.createElement("button");
+	newButton.textContent = newAction + " - " + pointValue;
+	div.appendChild(newButton);
 }
